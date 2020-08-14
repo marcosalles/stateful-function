32cc598da6882cf619ef6b9eb6d8fcb96703ba10
Marco Salles <marco.salles@geekie.com.br>
2020-08-13 21:44:55 -0300

    Updates

---
+++ precache-manifest.e3c5635afcc29f2d25f5920c2481307d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a023c44a6e835acd7a841f642d85fbd",
+    "url": "/stateful-function/index.html"
+  },
+  {
+    "revision": "198a7fd4de97265f3838",
+    "url": "/stateful-function/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a1788862147f998de625",
+    "url": "/stateful-function/static/js/2.f716960a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stateful-function/static/js/2.f716960a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "198a7fd4de97265f3838",
+    "url": "/stateful-function/static/js/main.b7ae637d.chunk.js"
+  },
+  {
+    "revision": "3ccc4acd962b252b8da3",
+    "url": "/stateful-function/static/js/runtime-main.af6d0810.js"
+  }
+]);
